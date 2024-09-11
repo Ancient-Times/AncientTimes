39d0e8754aa867c973c0a97a37a85eee33f56d7d
Delu <0.amigo.abc@gmail.com>
2024-09-11 20:12:35 +0200

    mard flower - adding block ~ More progress

--- src/main/java/com/delu/ancienttimes/datagen/ModBlockSTatesProvider.java
+++ src/main/java/com/delu/ancienttimes/datagen/ModBlockSTatesProvider.java
@@ -2,8 +2,13 @@ package com.delu.ancienttimes.datagen;
 
 import com.delu.ancienttimes.AncientTimes;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.block;
 
 public class ModBlockSTatesProvider extends BlockStateProvider {
 
@@ -13,6 +18,14 @@ public class ModBlockSTatesProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        
     }
+
+    protected void makeMardFlowerModel(Block block) {
+        getVariantBuilder(block).forAllStates(state -> {
+            String name = ForgeRegistries.BLOCKS.getKey(block).getPath();
+            int age = state.getValue(BlockStateProperties.AGE_3);
+            String modelName = ForgeRegistries.BLOCKS.getKey(block).getPath();
+        });
+    }
+
 }
